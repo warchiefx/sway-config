ac9064c51f77719f3544b5bd4d2f1fdc64f9dda3
Ricardo Restituyo <warchiefx@gmail.com>
2019-10-30 21:25:09 -0400

    Add swaylock config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 mkdir -p ~/.config/waybar/
 mkdir -p ~/.config/dunst/
+mkdir -p ~/.config/swaylock/
 ln -sf `pwd`/waybar-config.json ~/.config/waybar/config
 ln -sf `pwd`/style.css ~/.config/waybar/style.css
 ln -sf `pwd`/dunstrc ~/.config/dunst/dunstrc
+ln -sf `pwd`/swaylock.conf ~/.config/swaylock/config
