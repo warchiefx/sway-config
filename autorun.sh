b63d35d64256d64de7d83180dd7f5340c0f1bb64
Ricardo Restituyo <rrestituyo@vivial.net>
2019-11-01 14:19:58 -0400

    Add keychain integration

--- autorun.sh
+++ autorun.sh
@@ -7,6 +7,4 @@ function run {
   fi
 }
 
-run /usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
-run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run setxkbmap -layout us -variant altgr-intl -option ctrl:swapcaps
--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/zsh
 
 mkdir -p ~/.config/waybar/
 ln -sf `pwd`/waybar/waybar-config.json ~/.config/waybar/config
 ln -sf `pwd`/waybar/style.css ~/.config/waybar/style.css
+
+mkdir -p ~/.config/systemd/user/
+ln -sf `pwd`/systemd/keychain.service ~/.config/systemd/user/keychain.service
+systemctl --user enable keychain.service
