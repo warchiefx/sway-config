b7d6db3b7e7035a829bc768ddbbc3d3734828ec1
Ricardo Restituyo <rrestituyo@vivial.net>
2019-11-01 13:54:03 -0400

    More reorganization

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 mkdir -p ~/.config/waybar/
-ln -sf `pwd`/waybar-config.json ~/.config/waybar/config
-ln -sf `pwd`/style.css ~/.config/waybar/style.css
+ln -sf `pwd`/waybar/waybar-config.json ~/.config/waybar/config
+ln -sf `pwd`/waybar/style.css ~/.config/waybar/style.css
