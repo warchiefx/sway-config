dd2175b589f005d2574d963d52ed1b4432e1c2c4
Ricardo Restituyo <warchiefx@gmail.com>
2019-10-31 20:45:13 -0400

    Revamp apps to load and some rules

--- autorun.sh
+++ autorun.sh
@@ -9,8 +9,4 @@ function run {
 
 run /usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-run nm-applet
-run volumeicon
-run gnome-screensaver
-run solaar
 run setxkbmap -layout us -variant altgr-intl -option ctrl:swapcaps
