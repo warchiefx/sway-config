d3c12fdf88b9470f9bb2dbf3d53ec2860a7c3973
Ricardo Restituyo <rrestituyo@vivial.net>
2019-11-01 13:52:11 -0400

    Organize things a little bit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
 
 mkdir -p ~/.config/waybar/
-mkdir -p ~/.config/dunst/
-mkdir -p ~/.config/swaylock/
 ln -sf `pwd`/waybar-config.json ~/.config/waybar/config
 ln -sf `pwd`/style.css ~/.config/waybar/style.css
-ln -sf `pwd`/dunstrc ~/.config/dunst/dunstrc
-ln -sf `pwd`/swaylock.conf ~/.config/swaylock/config
