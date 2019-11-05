3e3d46431763f78f2a7748903c903a691589aaf7
Ricardo Restituyo <warchiefx@gmail.com>
2019-11-04 20:56:32 -0400

    Add mako specific config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ mkdir -p ~/.config/waybar/
 ln -sf `pwd`/waybar/waybar-config.json ~/.config/waybar/config
 ln -sf `pwd`/waybar/style.css ~/.config/waybar/style.css
 
+# Notification DAemon
+mkdir -p ~/.config/mako
+ln -sf `pwd`/mako.conf ~/.config/mako/config
+
 # SSH Agent
 mkdir -p ~/.config/systemd/user/
 ln -sf `pwd`/systemd/keychain.service ~/.config/systemd/user/keychain.service
